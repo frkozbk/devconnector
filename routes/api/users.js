6ee30916db1d4175c28d6667c3885091b7efdec7
frkozbk <ozbekfaruk12@gmail.com>
2019-03-26 00:09:46 +0300

    Jwt passport strateji eklendi

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,97 @@
 const express = require('express')
 const router = express.Router();
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
+// Load User model
+const User = require('../../models/User')
+const passport = require('passport')
 
 //  @route GET api/users/test
 //  @desc Tests post route
 //  @access Public
 router.get('/test', (req, res) => {
     res.json({ msg: 'Users Works' })
+})
+//  @route POST api/users/register
+//  @desc Register user
+//  @access Public
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'Email already exists' })
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //Size
+                    r: 'pg',  //Rating
+                    d: 'mm' // Default
+                })
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err))
+                    })
+                })
+            }
+        })
+})
+//  @route GET api/users/login
+//  @desc Login user / Returning JWT Token
+//  @access Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            // Check for user
+            if (!user) {
+                return res.status(404).json({ email: 'User not found' })
+            }
+            // Check Password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        // User Matced
+                        const payload = { id: user.id, name: user.name, avatar: user.avatar } // Create JWT Token
+
+                        // Then Sign Token
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 360000 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                })
+                            })
+
+                    }
+                    else {
+                        return res.status(400).json({ password: 'Password incorrect' })
+                    }
+                })
+
+        })
+})
+//  @route GET api/users/current
+//  @desc Return current user
+//  @access Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json(req.user)
 })
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
@@ -9,6 +11,11 @@ const posts = require('./routes/api/posts')
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
@@ -17,6 +24,11 @@ mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err))
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport)
 
 app.get('/', (req, res) => {
     res.send('Hello')
