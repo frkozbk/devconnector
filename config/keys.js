6508d9bb1c5722ae2e97fbea12c7c827e849234a
frkozbk <ozbekfaruk12@gmail.com>
2019-03-29 17:36:05 +0300

    login logout redux

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://faruk:faruk@dev-shard-00-00-xgixl.mongodb.net:27017,dev-shard-00-01-xgixl.mongodb.net:27017,dev-shard-00-02-xgixl.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin&retryWrites=true',
-    secretOrKey: 'secret'
+  mongoURI: 'mongodb://faruk:faruk@dev-shard-00-00-xgixl.mongodb.net:27017,dev-shard-00-01-xgixl.mongodb.net:27017,dev-shard-00-02-xgixl.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: 'secret'
 };
-
--- server.js
+++ server.js
@@ -1,47 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const passport = require('passport')
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
-
-
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
 // Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
-
-
-
-
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
